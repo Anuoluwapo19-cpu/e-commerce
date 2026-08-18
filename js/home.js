586da98279a633dc10b4a6b689c87557e1e264c4
Anuoluwapo <anuoluwaporaji32@gmail.com>
2026-08-18 16:23:29 +0100

    Add JavaScript functionality to cart

--- js/home.js
+++ js/home.js
@@ -19,12 +19,55 @@ function renderProducts(category) {
             `<button class ="btn add-to-cart-btn" data-id = '${product.id}'>Add to cart</button>`
 
             grid.appendChild(card)
+
       }
+      let addButtons = document.querySelectorAll('.add-to-cart-btn')
+      addButtons.forEach(function(button){
+         button.addEventListener('click', function(){
+            let productId = Number(this.getAttribute('data-id'))
+            addToCart(productId)
+
+         })
+
+      })
+
 }
 
 function addToCart(productId) {
-    
+    let product = null;
+    for(let i= 0; i<products.length; i++) {
+          if(products[i].id === productId){
+            product = products[i]
+            break
+          }
+    };
+
+    if(!product) {
+        return 
+    };
+    let cart = getCart();
+    let alreadyInCart = false;
+    for(let j=0; j< cart.length; j++){
+       if (cart[j].id === product.id){
+        cart[j].quantity = cart[j].quantity+1
+        alreadyInCart = true;
+        break
+       }
+    }
+    if(!alreadyInCart){
+        cart.push({
+            id: product.id, 
+            name: product.name,
+            price: product.price,
+            image: product.image,
+            quantity: 1
+        })
+    }
+    saveCart(cart)
+    alert(`${product.name} was added to cart`)
+
 }
+
 
 
 let filterButtons = document.querySelectorAll('.filter-btn');
--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,16 @@
 function getCart(){
     let cartText = localStorage.getItem('boutiqueCart');
     if(cartText) {
+        try{
         return JSON.parse(cartText)
+        } catch(e){
+            localStorage.removeItem('boutiqueCart');
+        }
     }
 
     return [];
+}
+
+function saveCart(cart) {
+    localStorage.setItem('boutiqueCart', JSON.stringify(cart));
 }
